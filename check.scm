135fabd66d2b5b23f58ca6acbd3edba10ce0ee9e
Kooda <kooda@upyum.com>
2016-12-21 15:16:18 +0100

    Add a crude “system file tasks” implementation.

--- check.scm
+++ check.scm
@@ -1,4 +1,37 @@
-(use scheme2c-compatibility xlib)
+(use scheme2c-compatibility xlib posix)
+
+;; System file tasks
+
+(define *tasks* '())
+(define *tasks-enabled* #f)
+
+(define (define-system-file-task file idle-task file-task)
+  (set! *tasks* (cons (list file idle-task file-task) *tasks*)))
+
+(define (enable-system-file-tasks flag)
+  (set! *tasks-enabled* flag))
+
+(define (wait-system-file exp)
+  (void))
+
+(define (run-tasks escape-file)
+  (let ((ready (file-select (cons escape-file (map car *tasks*)) #f)))
+    (if (member escape-file ready)
+        #t
+        (begin
+          (for-each
+            (lambda (fd)
+              ((caddr (assoc fd *tasks*))))
+            ready)
+          (run-tasks escape-file)))))
+
+(define (ezd-read)
+  (when (and (not (char-ready?)) *tasks-enabled*)
+    (let ((idle-tasks (map cadr *tasks*)))
+      (for-each (lambda (thunk) (thunk)) idle-tasks)
+      (run-tasks (port->fileno (current-input-port)))))
+  (read))
+
 
 (define putprop put!)
 (define getprop get)
